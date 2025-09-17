c3f996757b29617c5616371eb2f5b20dee9598e0
Adam C. Guthrie <adam.c.guthrie@lumen.com>
2025-09-17 14:46:36 -0500

    Updated internal function definitions

--- src/dotenv.c
+++ src/dotenv.c
@@ -63,12 +63,12 @@ int getline(char **lineptr, size_t *n, FILE *stream) {
 #endif
 
 /* strtok_r() won't remove the whole ${ part, only the $ */
-#define remove_bracket(name) name + 1
+#define dotenv_remove_bracket(name) name + 1
 
-#define remove_space(value) value + 1
+#define dotenv_remove_space(value) value + 1
 
 
-static char *concat(char *buffer, char *string)
+static char *dotenv_concat(char *buffer, char *string)
 {
     if (!buffer) {
         return strdup(string);
@@ -83,7 +83,7 @@ static char *concat(char *buffer, char *string)
     return buffer;
 }
 
-static bool is_nested(char *value)
+static bool dotenv_is_nested(char *value)
 {
     return strstr(value, "${") && strstr(value, "}");
 }
@@ -92,7 +92,7 @@ static bool is_nested(char *value)
  * @example With TEST_DIR=${BASE_DIR}/.test the first strtok_r call will return
  * BASE_DIR}/.test instead of NULL, or an empty string
  */
-static char *prepare_value(char *value)
+static char *dotenv_prepare_value(char *value)
 {
     char *new = malloc(strlen(value) + 2);
     sprintf(new, " %s", value);
@@ -100,22 +100,22 @@ static char *prepare_value(char *value)
     return new;
 }
 
-static char *parse_value(char *value)
+static char *dotenv_parse_value(char *value)
 {
-    value = prepare_value(value);
+    value = dotenv_prepare_value(value);
 
     char *search = value, *parsed = NULL, *tok_ptr;
     char *name;
 
-    if (value && is_nested(value)) {
+    if (value && dotenv_is_nested(value)) {
         while (1) {
-            parsed = concat(parsed, strtok_r(search, "${", &tok_ptr));
+            parsed = dotenv_concat(parsed, strtok_r(search, "${", &tok_ptr));
             name = strtok_r(NULL, "}", &tok_ptr);
 
             if (!name) {
                 break;
             }
-            parsed = concat(parsed, getenv(remove_bracket(name)));
+            parsed = dotenv_concat(parsed, getenv(dotenv_remove_bracket(name)));
             search = NULL;
         }
         free(value);
@@ -125,7 +125,7 @@ static char *parse_value(char *value)
     return value;
 }
 
-static bool is_commented(char *line)
+static bool dotenv_is_commented(char *line)
 {
     if ('#' == line[0]) {
         return true;
@@ -141,35 +141,35 @@ static bool is_commented(char *line)
     return false;
 }
 
-static void set_variable(char *name, char *original, bool overwrite)
+static void dotenv_set_variable(char *name, char *original, bool overwrite)
 {
     char *parsed;
 
     if (original) {
-        parsed = parse_value(original);
-        setenv(name, remove_space(parsed), overwrite);
+        parsed = dotenv_parse_value(original);
+        setenv(name, dotenv_remove_bracket(parsed), overwrite);
 
         free(parsed);
     }
 }
 
-static void parse(FILE *file, bool overwrite)
+static void dotenv_parse(FILE *file, bool overwrite)
 {
     char *name, *original, *line = NULL, *tok_ptr;
     size_t len = 0;
 
     while (-1 != getline(&line, &len, file)) {
-        if (!is_commented(line)) {
+        if (!dotenv_is_commented(line)) {
             name = strtok_r(line, "=", &tok_ptr);
             original = strtok_r(NULL, "\n", &tok_ptr);
 
-            set_variable(name, original, overwrite);
+            dotenv_set_variable(name, original, overwrite);
         }
     }
     free(line);
 }
 
-static FILE *open_default(const char *base_path)
+static FILE *dotenv_open_default(const char *base_path)
 {
     char path[512];
     sprintf(path, "%s/.env", base_path);
@@ -179,7 +179,7 @@ static FILE *open_default(const char *base_path)
 
 int env_load(const char *path, bool overwrite)
 {
-    FILE *file = open_default(path);
+    FILE *file = dotenv_open_default(path);
 
     if (!file) {
         file = fopen(path, "rb");
@@ -188,7 +188,7 @@ int env_load(const char *path, bool overwrite)
             return -1;
         }
     }
-    parse(file, overwrite);
+    dotenv_parse(file, overwrite);
     fclose(file);
 
     return 0;
